004ab3813446ab10349dd88393352b696a504304
Nicolaswg <nigonbol123@gmail.com>
2023-08-14 13:29:55 -0400

    Create profile tab for user || download shadcn tab ui components || Add user action for finding user threads by id || Add correct comment styling and spaces to match ui || Fix redirection to onboaring in home page when the user onboarded param is not true || Fix profile tab path to work correclty

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,18 @@
 import ThreadCard from "@/components/cards/ThreadCard";
 import { fetchPosts } from "@/lib/actions/thread.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
 export default async function Home() {
   const result = await fetchPosts(1, 30);
+
   const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
